a36793d9eade28cb75e135167d3e5386134fabf8
German Robayo <germanrobayo33@gmail.com>
2022-02-03 01:35:59 -0400

    add relationship between ref types and user

--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
 import email
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from backend.models import Disease
+from backend.models import Disease, ReferenceType
 # Create your models here.
 
 class User(AbstractUser):
     email = models.EmailField('email address', unique=True)
     disease_of_interest = models.ManyToManyField(to=Disease, related_name='+')
+    reference_types_of_interest = models.ManyToManyField(to=ReferenceType, related_name='+')
     # REQUIRED_FIELDS = ['username']
